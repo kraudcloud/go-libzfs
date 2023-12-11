d31aadf63213d6fe20d365136535c6c37921eb4c
Arvid E. Picciani <arvid@kraud.cloud>
2023-12-11 19:50:20 +0100

    fix for musl

--- common.go
+++ common.go
@@ -7,7 +7,7 @@
 package zfs
 
 /*
-#cgo CFLAGS: -I /usr/include/libzfs -I /usr/include/libspl -DHAVE_IOCTL_IN_SYS_IOCTL_H -D_GNU_SOURCE
+#cgo CFLAGS: -I /usr/include/libzfs -I /usr/include/libspl -DHAVE_IOCTL_IN_SYS_IOCTL_H -D_GNU_SOURCE -D_LARGEFILE64_SOURCE
 #cgo LDFLAGS: -lzfs -lzpool -lnvpair
 
 #include <stdlib.h>
